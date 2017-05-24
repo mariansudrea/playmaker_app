225fb58a61ced33081ce44e08a96409f14f6bee5
Marian Udrea <smenarru@gmail.com>
2017-05-23 23:29:35 -0700

    play_prototype_rewritten

--- js/test/testController.js
+++ js/test/testController.js
@@ -3,20 +3,40 @@ app.controller('testController', ['$scope','$timeout', function($scope,$timeout)
 	$scope.testFunction = function(){
 		return "it works."
 	}
-	
+//	General purpose variables
+//
+	$scope.new_play_model = {};
+	$scope.team1_input = 1;
+	$scope.team2_input = 1;
+	$scope.team1_temp_color = "red";
+	$scope.team2_temp_color = "blue";
+	$scope.frames_input = 1;
+	$scope.frames_input_validated = 1;
+
+	$scope.team1_temp_style = {}; 	
+	$scope.team2_temp_style = {}; 	
+
+
 	$scope.players = [];
 	$scope.grid_size = [];
 	$scope.grid_size.length = 200;	
 	$scope.selected_player = null;
-
-
+	$scope.mode = "edit";
+	$scope.modal_visible = false;
+	$scope.team1_preview = [ "" ];
+	$scope.team2_preview = [ "" ];
+	$scope.new_play_model.team1 = $scope.team1_preview;
+	$scope.new_play_model.team2 = $scope.team2_preview;
 // Grid init with X/Y coordinates for each.
 
 	for (var i = 0; i < $scope.grid_size.length ; i++ ){
 		$scope.grid_size[i] = { posX: i % 20,
-								posY: Math.floor(i/20)					
+								posY: Math.floor(i/20),
+								occupied: false						
 			}; 
 	}
+	$scope.default_positions1 = [ [3,1],[3,4],[3,7],[6,3],[6,8],[8,9],[0,4] ];
+	$scope.default_positions2 = [ [10,1],[10,4],[10,8],[12,2],[12,5],[12,8],[17,4] ];
 
 //	Player Prototypes / Mocks
 
@@ -41,6 +61,7 @@ app.controller('testController', ['$scope','$timeout', function($scope,$timeout)
 	}
 	$scope.player2 = {
 		default_color: '#ff0000',
+		team: 	1,
 		jersey_number: 14,
 		default_shadow: 'none',
 		current_style:  { 	top:		"120px",
@@ -75,9 +96,77 @@ app.controller('testController', ['$scope','$timeout', function($scope,$timeout)
 			}
 		
 	}
+	$scope.addPlayer = function(color,default_position,number){
+		var new_current_frames = [];
+
+		for ( var i = 0 ; i < $scope.frames_input_validated ; i++) {
+			new_current_frames.push({
+				posX: default_position[0],
+				posY: default_position[1],
+				highlighted: false
+			});
+		}
+		var new_current_style = {  
+						'background-color': color,
+						'box-shadow': this.calculated_shadow,
+						//add more dialects
+						'border': "1 px solid transparent", 
+						top: ( default_position[1] * 40 ) + "px",
+						left: ( default_position[0] * 40 ) + "px",
+			}
+
+		$scope.players.push({
+			default_color: 		color,
+			jersey_number: 		number,
+			default_shadow: 	'none',
+			current_style: 		new_current_style,
+			frames: 			new_current_frames,
+			transitionTo:	function(index){
+				if ( this.frames[index].highlighted == true ){
+					console.log('%c true value found for highlighted', 'color:red');
+					console.log('this.highlighted = ' + this.frames[index].highlighted);
+//					this.calculated_shadow = "0px 0px 20px 6px rgba(255, 0, 0, 1)"
+					this.calculated_shadow = "0px 0px 20px 6px "+ this.default_color;
+				} else {
+					this.calculated_shadow = this.default_shadow;
+				}
+				this.current_style= {
+						top: ( this.frames[index].posY * 40 ) + "px",
+						left: ( this.frames[index].posX * 40 ) + "px",
+						'background-color': this.default_color,
+						'box-shadow': this.calculated_shadow,
+						//add more dialects
+						'border': ( this.frames[index].highlighted == true ? '1px solid #38cc24' : "1 px solid transparent" )
+					}
+			}
+			 
+		})
+	}
+
+	$scope.generateNewPlay = function(){
+		console.log('generating new play');
+		$scope.modal_visible = false;
+		$scope.frames = [];
+		for ( var i = 0 ; i < $scope.frames_input_validated ; i++ ) {
+			$scope.frames.push(i);
+		}
+		$scope.players = [];
+		for ( i = 0 ; i < $scope.team1_preview.length; i++ ) {
+			$scope.addPlayer( $scope.team1_temp_color, $scope.default_positions1[i], i )
+		}
+		for ( i = 0 ; i < $scope.team2_preview.length ; i++ ) {
+			$scope.addPlayer( $scope.team2_temp_color, $scope.default_positions2[i], i )
+		}
+		$scope.addPlayer( "aqua", [9,5], 22 )
+
+	//	for ( i = 
+	}
+
+
+	
 //  ************END PLAYER PROTOTYPE
 //
-	$scope.player1.position = "p4";
+//	$scope.player1.position = "p4";
 
 	$scope.players.push($scope.player1);
 	$scope.players.push($scope.player2);
@@ -103,6 +192,7 @@ app.controller('testController', ['$scope','$timeout', function($scope,$timeout)
 			}
 		}
 		$scope.current_frame_index = destination_frame_index;
+		$scope.play_data_model = JSON.stringify($scope.players);
 	}
 
 	$scope.playback = function(){
@@ -140,15 +230,65 @@ app.controller('testController', ['$scope','$timeout', function($scope,$timeout)
 	$scope.moveSelectedPlayer = function(grid_square_object){
 		if ( $scope.selected_player != null ) {
 			console.log('move selected function triggered');
-			$scope.selected_player.frames[$scope.current_frame_index].posX = grid_square_object.posX;
-			$scope.selected_player.frames[$scope.current_frame_index].posY = grid_square_object.posY;
+			if ( $scope.mode == "edit" ){
+				$scope.selected_player.frames[$scope.current_frame_index].posX = grid_square_object.posX;
+				$scope.selected_player.frames[$scope.current_frame_index].posY = grid_square_object.posY;
+			} else {
+				for ( var i = $scope.current_frame_index ; i < $scope.frames.length ; i++ ){
+					$scope.selected_player.frames[i].posX = grid_square_object.posX;
+					$scope.selected_player.frames[i].posY = grid_square_object.posY;
+				}
+			}
 			$scope.updateSelectedPlayer();
 		}
 	}
 	
-	$scope.toggleHighlighted = function(){
-		//$scope.selected_player.frames[current_frame_index].highlighted = ( $scope.selected_player.frames[current_frame_index] == true ? false : true )
-		$scope.updateSelectedPlayer();
+
+	$scope.toggleMode = function(){
+		$scope.mode = ( $scope.mode == "edit" ? "create" : "edit" );
 	}
+
+	$scope.updatePreviewTeams = function(){
+		var team1_input_validated = parseInt($scope.team1_input);
+		if ( isNaN(team1_input_validated) ) {
+			team1_input_validated = 1;
+		} else if ( team1_input_validated > 7 ) {
+			team1_input_validated = 7;
+		} else if (team1_input_validated < 1 ) {
+			team1_input_validated = 1;
+		} 
+		var team2_input_validated = parseInt($scope.team2_input);
+		if ( isNaN(team2_input_validated) ) {
+			team2_input_validated = 1;
+		} else if ( team2_input_validated > 7 ) {
+			team2_input_validated = 7;
+		} else if (team2_input_validated < 1 ) {
+			team2_input_validated = 1;
+		} 
+		var frames_input_validated = parseInt($scope.frames_input);
+		if ( isNaN(frames_input_validated) ) {
+			frames_input_validated = 1;
+		} else if ( frames_input_validated > 15 ) {
+			frames_input_validated = 15;
+		} else if (frames_input_validated < 1 ) {
+			frames_input_validated = 1;
+		} 
+		console.log('working');	
+		console.log('Validated Inputs:' + team1_input_validated + " " + team2_input_validated + " " + frames_input_validated + " " + $scope.team1_temp_color + " " + $scope.team2_temp_color);	
+		$scope.team1_preview = [];
+		$scope.team2_preview = [];
+		for ( var i = 0 ; i < team1_input_validated ; i++ ) {
+			$scope.team1_preview.push("");	
+		}
+		for ( var i = 0 ; i < team2_input_validated ; i++ ) {
+			$scope.team2_preview.push("");	
+		}
+		$scope.team1_temp_style['background-color'] = $scope.team1_temp_color; 	
+		$scope.team2_temp_style['background-color'] = $scope.team2_temp_color; 	
+
+		$scope.frames_input_validated = frames_input_validated;
+//		$scope.team1_temp_position !!!!!!!!!  = 	
+	}
+
 }])
 
