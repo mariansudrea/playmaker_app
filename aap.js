c432857c2cd681d36b9d1b1ac4a9c0860dacae8c
Marian Udrea <smenarru@gmail.com>
2020-04-16 17:48:31 -0700

    old demo backup and ec2 transfer

--- aap.js
+++ aap.js
@@ -1,1 +1,1 @@
-app=angular.module("soccer_playmaker",["ui.router"]),app.config(["$stateProvider","$urlRouterProvider",function(r,e){r.state("app",{url:"/app",templateUrl:"/js/test/_test.html",controller:"appController"}),e.otherwise("app")}]);
+app=angular.module("soccer_playmaker",["ui.router"]),app.config(["$stateProvider","$urlRouterProvider",function(r,e){r.state("app",{url:"/app",templateUrl:"./js/test/_test.html",controller:"appController"}),e.otherwise("app")}]);
